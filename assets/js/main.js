243d6049177fbc3579d806874fcd6b15572c1537
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-13 16:48:12 +0100

    add for cile to check instead of includes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,21 +7,32 @@ console.log(emailDatabase);
 
 document.querySelector('button').addEventListener('click', function () {
 
-    if (emailDatabase.includes('pietro@gmail.com') && userEmail === ('pietro@gmail.com')) {
-        console.log('ciao pietro');
-        // se ti chiami pietro@gmail.com ed é dentro l'array allora okey
+    // if (emailDatabase.includes('pietro@gmail.com') && userEmail === ('pietro@gmail.com')) {
+    //     console.log('ciao pietro');
+    //     // se ti chiami pietro@gmail.com ed é dentro l'array allora okey
 
-    } else if (emailDatabase.includes('giuliano@gmail.com') && userEmail === ('giuliano@gmail.com')) {
-        console.log('ciao Giuliano');
+    // } else if (emailDatabase.includes('giuliano@gmail.com') && userEmail === ('giuliano@gmail.com')) {
+    //     console.log('ciao Giuliano');
 
-    } else if (emailDatabase.includes('donato@gmail.com') && userEmail === ('donato@gmail.com')) {
-        console.log('ciao Donato');
+    // } else if (emailDatabase.includes('donato@gmail.com') && userEmail === ('donato@gmail.com')) {
+    //     console.log('ciao Donato');
 
-    } else if (emailDatabase.includes('fabio@gmail.com') && userEmail === ('donato@gmail.com')) {
-        console.log('ciao Fabio');
+    // } else if (emailDatabase.includes('fabio@gmail.com') && userEmail === ('donato@gmail.com')) {
+    //     console.log('ciao Fabio');
 
-    } else {
-        console.log('sei nuovo da queste parti?')
+    // } else {
+    //     console.log('sei nuovo da queste parti?')
+
+    // }
+
+
+    for (let i = 0; i < emailDatabase.length; i++) {
+        if (emailDatabase[i] == userEmail) {
+            console.log("benvenuto")
+        } else {
+            console.log('sei nuovo da queste parti ?')
+        }
+
 
     }
 
