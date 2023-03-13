3e9d8ffbfb83669e8ccea8bde1983a474c42f4ab
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-13 18:12:49 +0100

    working js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,40 +27,60 @@ document.querySelector('button').addEventListener('click', function () {
 
     // }
 
+    let hasAccess = false;
+    // crei una variabile esterna
 
     for (let i = 0; i < emailDatabase.length; i++) {
         if (emailDatabase[i] == userEmail) {
-            console.log("benvenuto")
-
-        } else if (emailDatabase[i] != userEmail) {
-            // console.log('sei nuovo da queste parti ?')
-
-        } else {
-
+            hasAccess = true;
+            // la cambi se corrisponde
         }
-
-        // -----
-
     }
 
-}
-)
+    // fuori dal loop
+    // scrivi una cosa se é  vero e un altra se é falso
+
+    if (hasAccess == true) {
+        console.log("Benvenuto");
+        document.getElementById("warning").innerHTML = "Benvenuto";
+
+    } else if (hasAccess == false) {
+        console.log("Sei nuovo da queste parti ?");
+        document.getElementById("warning").innerHTML = "Sei nuovo da queste parti ?";
+    }
+
+
+
+})
+
+
 
 
 // --------- Dice Games
 
-const userDice = Math.floor(Math.random() * 6) + 1
-console.log(userDice)
-// dado dell'utente
+document.getElementById('start').addEventListener('click', function () {
 
-const enemyDice = Math.floor(Math.random() * 6) + 1
-console.log(enemyDice)
+    const userDice = Math.floor(Math.random() * 6) + 1
+    console.log(userDice);
+    // dado dell'utente
 
-// dado del pc
+    const enemyDice = Math.floor(Math.random() * 6) + 1
+    console.log(enemyDice);
 
-if (userDice >= enemyDice) {
-    console.log('hai vinto')
+    document.getElementById('result').innerHTML = `Questo é il tuo risultato ${userDice} <br>
+    E questo é quello del computer ${enemyDice}`;
 
-} else if (userDice <= enemyDice) {
-    console.log('hai perso')
-}
+    // dado del pc
+
+    if (userDice >= enemyDice) {
+        console.log('hai vinto');
+        document.getElementById('winner').innerHTML = "Hai vinto";
+
+    } else if (userDice <= enemyDice) {
+        console.log('hai perso');
+        document.getElementById('winner').innerHTML = "Hai perso";
+    }
+
+})
+
+
